ee41f58728674dabed2abda2aa6bf6cad022a3fe
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2022-12-13 10:36:39 -0700

    Add pseudocode and code

--- challenge1.rb
+++ challenge1.rb
@@ -1,1 +1,13 @@
-##
+## Given an array of strings, return only the strings that have exactly 4 characters.
+
+# 1. write code that iterates over each element in the given array
+# 2. write a conditional that chooses only those elements with 4 characters in length
+# 3. write code to print out "strings" of those elements with 4 characters
+
+strings = ["laptop", "desk", "TV", "film", "jack", "philadelphia"]
+
+strings.each do |string|
+    if string.length == 4
+        puts string
+    end
+end
