c26a7cc9ff2c730505f41530897cbf523215ae33
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2022-12-13 10:46:01 -0700

    Add pseudocode and code

--- challenge2.rb
+++ challenge2.rb
@@ -1,1 +1,11 @@
-##
+##Start with an array of strings with a mix of uppercase and lowercase letters. Print every word in all lowercase letters.
+
+# 1. Write code to iterate over each element in the array
+# 2. Use .downcase method so that the code will print each element in all lowercase letters
+
+
+foods = ["APPles", "oRange", "BANANa", "PoTaTo", "CREAM"]
+
+foods.each do |food|
+    puts food.downcase
+end
